1f2a6e5143de0f17bd090612ffe49914d00745dc
Jom006 <jom006@gmail.com>
2013-12-05 15:50:57 +0000

    SetUps

--- index.php
+++ index.php
@@ -7,13 +7,22 @@
 	<meta name="keywords"content="comptabilité">
 	<meta name="author" content="jom006">
 	<link rel="shortcut icon" href="images/shortcuticon.png">
-	<link type="text/css" rel="stylesheet" href="style.css">
+	<link type="text/css" rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 	<script type="text/javascript" src="js/jquery.js" ></script>
 	<script type="text/javascript" src="js/js.js" ></script>
 	<script type="text/javascript" src="zoombox/zoombox.js"></script>
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="angular.min.js"></script>
+	<!--[if lt IE 9]>
+	<script src="js/html5shiv.js"></script>
+	<![endif]-->
 </head>
 <body>
 
+	<header>
+		<nav></nav>
+	</header>
 
 </body>
 </html>
