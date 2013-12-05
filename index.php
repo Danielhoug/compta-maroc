2c2789489e10c4b2bc17c411b299e5db3860181c
Jom006 <jom006@gmail.com>
2013-12-05 17:49:24 +0000

    nothing special just saving

--- index.php
+++ index.php
@@ -1,28 +1,46 @@
-<!doctype html>
+<!DOCTYPE html>
 <html>
-<head>
-	<title>Compta</title>
+<head lang="fr">
+	<title>GoCompta</title>
 	<meta charset="utf-8">
-	<meta name="description" content="Application Comptabilité marocaine">
-	<meta name="keywords"content="comptabilité">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="Application Comptabilité Marocaine">
+	<meta name="keywords"content="comptabilité marocaine">
 	<meta name="author" content="jom006">
 	<link rel="shortcut icon" href="images/shortcuticon.png">
 	<link type="text/css" rel="stylesheet" href="css/style.css">
 	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap-theme.min.css">
 	<script type="text/javascript" src="js/jquery.js" ></script>
 	<script type="text/javascript" src="js/js.js" ></script>
-	<script type="text/javascript" src="zoombox/zoombox.js"></script>
 	<script type="text/javascript" src="js/bootstrap.min.js"></script>
 	<script type="text/javascript" src="angular.min.js"></script>
 	<!--[if lt IE 9]>
-	<script src="js/html5shiv.js"></script>
+		<script src="js/html5shiv.js"></script>
+		<script src="js/respond.min.js"></script>
 	<![endif]-->
 </head>
 <body>
 
-	<header>
-		<nav>test</nav>
-	</header>
+	<div class="navbar navbar-inverse navbar-static-top">
+		<div class="container">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+			<a class="navbar-brand" href="#">Project name</a>
+			</div>
+			<div class="navbar-collapse">
+				<ul class="nav navbar-nav navbar-left">
+					<li><a href="#">Menu</a></li>
+					<li><a href="#">Options</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
 
 </body>
 </html>
